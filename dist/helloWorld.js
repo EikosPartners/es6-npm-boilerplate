8407fd721694637981605f86ac93e3626c0280a8
Zachary Donato <zdonato@stevens.edu>
2016-06-17 12:21:16 -0400

    Added source maps to build.

--- dist/helloWorld.js
+++ dist/helloWorld.js
@@ -1,13 +1,19 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 require("./helloWorld.html");
 
+/**
+ * An example module.
+ * @module helloWorld
+ */
+
 exports.default = {
-    sayHello: function sayHello() {
-        console.log("Hello World");
-    }
+  sayHello: function sayHello() {
+    console.log("Hello World");
+  }
 };
+//# sourceMappingURL=helloWorld.js.map
